313e089409a1bdb0b2d5041b50964c04a18585e1
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 00:15:30 +0530

    updated vercel

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'https://trello-backend-vert.vercel.app', 
+app.use(cors(
+  {
+  origin: ['https://trello-backend-vert.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+  }
+));
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
