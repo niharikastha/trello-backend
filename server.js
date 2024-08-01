1500ff45eece0544a390ac41b88687ac483a5a73
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 09:51:05 +0530

    added cors

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use(cors(
   {
   origin: ['https://trello-backend-vert.vercel.app'], 
