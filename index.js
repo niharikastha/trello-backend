399a2c42eff877ff3ceb7aba2cca38dd8a05d0d9
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 12:32:11 +0530

    changed server to index.js

--- index.js
+++ index.js
@@ -6,30 +6,21 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello',
-  })
-})
-app.use(bodyParser.json());
-app.use(cors());
 
-app.use(cors(
-  {
-  origin: '*', 
-  // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowRequestHeaders: "Content-Type, Authorization",
+app.use(bodyParser.json());
+app.use(cors({
+  origin: '*',
+  allowRequestHeaders: 'Content-Type, Authorization',
   credentials: true,
-  allRoutes : true
-  }
-));
+}));
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.log('MongoDB connection error:', err);
     process.exit(1);
   });
-
+  
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
@@ -39,6 +30,12 @@ app.use('/api/tasks', taskRoutes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ message: 'Something went wrong!' });
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello',
+  });
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
