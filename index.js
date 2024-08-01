8c27726c00e30efecd753915dda7b81277f4af06
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 12:59:14 +0530

    added vercel app to frontend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://trello-frontend-nine.vercel.app', 
   credentials: true, 
 }));
 
