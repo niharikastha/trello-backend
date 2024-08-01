845ccf1786612585c9fb1b0bb8cbf0f934e37f30
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 11:05:33 +0530

    changed cors

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello',
+  })
+})
 app.use(bodyParser.json());
 app.use(cors());
 
